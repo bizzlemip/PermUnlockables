69179a4c264b9ce64dd94ccb6f8f615c8b623e5e
bizzlemip <149446542+bizzlemip@users.noreply.github.com>
2023-11-07 04:15:31 -0500

    Add files via upload

---
+++ Patches/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using LC_API;
+using GameNetcodeStuff;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
+
+namespace PermUnlockables
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static bool oldhastime;
+        public static int MaxPlayers = 20;
+        public static bool instantiating;
+        private Harmony harmonymain;
+        private void Awake()
+        {
+            harmonymain = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmonymain.PatchAll();
+            Logger.LogInfo($"{PluginInfo.PLUGIN_GUID} has loaded!!!");
+        }
+
+        private void OnDestroy()
+        {
+            LC_API.ServerAPI.ModdedServer.SetServerModdedOnly();
+        }
+    }
+}
